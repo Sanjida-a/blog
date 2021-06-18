26c93ccfd2787e22d00203801eed3d806c4a7d21
Sanjida <sanjida_afreen@hotmail.com>
2021-06-18 00:04:42 -0400

    added category function; post and get

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,20 +1,20 @@
 const router = require("express").Router();
 const Category = require("../models/Category");
 
-router.post("/",async(req,res) =>{
+router.post("/", async(req,res) => {
     const newCat = new Category(req.body);
     try{
-        const savedCat = await newCat.savve();
+        const savedCat = await newCat.save();
         res.status(200).json(savedCat);
     }catch(err){
         rew.status(500).json(err);
     }
 });
 
-router.get("/:id",async(req,res)=>{
+router.get("/",async(req,res)=>{
     try{
-        const cat = await Category.findbyId(req.params.id);
-        res.status(200).json(cat);
+        const cats = await Category.find();
+        res.status(200).json(cats);
         }catch(err){
             res.status(500).json(err);
     }
